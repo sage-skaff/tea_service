0fb0fc029760940f043aa3b3f233426de62adde4
Sage Skaff <skaffsage@gmail.com>
2023-02-24 07:46:27 -0800

    Add test for post customer subscription error handling

--- app/controllers/api/v1/subscriptions_controller.rb
+++ app/controllers/api/v1/subscriptions_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::SubscriptionsController < ApplicationController
 
     def create
         subscription = Subscription.new(subscription_params)
-        if subscription.save!
+        if subscription.save
             render json: SubscriptionSerializer.new(subscription), status: 201
         else
             render json: { error: "Unable to add subscription"}, status: :bad_request
--- spec/requests/post_customer_subscription_spec.rb
+++ spec/requests/post_customer_subscription_spec.rb
@@ -17,4 +17,22 @@ RSpec.describe 'Post Customer Subscription' do
     expect(response).to have_http_status(201)
     expect(customer.subscriptions.first.title).to eq('Yerba Mate Blend')
   end
+
+  it 'errors when missing params' do
+    customer = Customer.create!(first_name: 'Ahsoka', last_name: 'Tano', email: 'atano@jedi.com', address: 'Space')
+    tea = Tea.create!(title: 'Yerba Mate', description: 'Energizing Yerba Mate Tea', temperature: 170, brew_time: 180)
+    subscription_params = {
+        "title": "Yerba Mate Blend",
+        "price": 15,
+        "status": "active"
+    }
+    post "/api/v1/customers/#{customer.id}/subscriptions", params: subscription_params.to_json,
+                                                           headers: { 'CONTENT_TYPE' => 'application/json', 'ACCEPT' => 'application/json' }
+
+    expect(response).to have_http_status(400)
+    
+    parsed_response = JSON.parse(response.body, symbolize_names: true)
+
+    expect(parsed_response[:error]).to eq("Unable to add subscription")
+  end
 end
